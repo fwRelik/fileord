a9017fd5845ea8088f6cf34e78776409d496b007
fwRelik <relik.fw@gmail.com>
2023-05-10 23:44:34 +0300

    Release v0.1.0

---
+++ affix.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+export class Affix {
+	constructor({ filename, alias = '', toExtname = '', prefix = '', postfix = '' }) {
+		this.filename = filename;
+		this.alias = alias;
+		this.toExtname = toExtname;
+		this.prefix = prefix;
+		this.postfix = postfix;
+	}
+
+	get extname() {
+		return path.extname(this.filename);
+	}
+
+	getAffix(uniq, counter) {
+		let name = '';
+
+		if (uniq) {
+			name = uuidv4();
+			counter = '';
+		} else name = this.alias ? this.alias : this.filename.replace(/\.[^/.]+$/, '');
+
+		return `${this.prefix}${name}${this.postfix}${counter}${this.extname}`;
+	}
+}
---
+++ index.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { program } from 'commander';
+import { Affix } from './affix.js';
+
+const bootstrap = async () => {
+	program.version('0.1.0', '-v, --version', 'Version');
+
+	program
+		.option('-pre, --prefix <value>', 'Prefix before filename', '')
+		.option('-post, --postfix <value>', 'Postfix after filename', '')
+		.option('-a, --alias <value>', 'Replacing the current filename', '')
+		.option('-p, --path <value>', 'Directory path', null)
+		.option('--uniq <boolean>', 'Filename is unified, while the current name and alias are ignored', false);
+
+	program.parse();
+	const { prefix, postfix, alias, path: pathName, uniq } = program.opts();
+	let counter = 1;
+
+	console.log(prefix, postfix, alias, pathName, uniq);
+	if (!pathName) throw new Error('Directory path not specified');
+
+	const files = await fs
+		.readdir(pathName, { encoding: 'utf-8' })
+		.then(res => res)
+		.catch(err => console.log(err));
+
+	Promise.all(
+		files.map(async filename => {
+			const affix = new Affix({
+				filename,
+				alias,
+				prefix,
+				postfix,
+			});
+			const currentPath = path.join(pathName, filename);
+			const newPath = path.join(pathName, `${affix.getAffix(uniq, counter++)}`);
+
+			return await fs
+				.rename(currentPath, newPath)
+				.then(res => {
+					console.log(`Renamed: ${filename} >> ${path.basename(newPath)}`);
+					return res;
+				})
+				.catch(err => console.log(err));
+		})
+	);
+};
+
+bootstrap();
